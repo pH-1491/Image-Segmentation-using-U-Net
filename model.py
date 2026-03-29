075eb7bb198ad71130acf5afbdb937166704cf8d
pH-1491 <priyeharsh19@gmail.com>
2026-03-29 19:25:54 +0530

    added the u-net model

--- model.py
+++ model.py
@@ -1,2 +1,81 @@
 import torch
 import torch.nn as nn
+from torch.nn import ConvTranspose2d
+
+
+def conv_block(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        #encoder part
+        self.enc1 = conv_block(3, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = conv_block(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.enc3 = conv_block(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.enc4 = conv_block(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        #bottle neck
+        self.bottleneck = conv_block(512, 1024)
+
+        #decoder
+        self.up4 = ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = conv_block(1024, 512)
+
+        self.up3 = ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = conv_block(512, 256)
+
+        self.up2 = ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = conv_block(256, 128)
+
+        self.up1 = ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = conv_block(128, 64)
+
+        self.final = nn.Conv2d(64, 1, kernel_size=1)
+
+
+    def forward(self, x):
+        # Encoder
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool1(e1))
+        e3 = self.enc3(self.pool2(e2))
+        e4 = self.enc4(self.pool3(e3))
+
+        # Bottleneck
+        b = self.bottleneck(self.pool4(e4))
+
+        # Decoder + Skip Connections
+        d4 = self.up4(b)
+        d4 = torch.cat([d4, e4], dim=1)  #skip connections
+        d4 = self.dec4(d4)
+
+        d3 = self.up3(d4)
+        d3 = torch.cat([d3, e3], dim=1)
+        d3 = self.dec3(d3)
+
+        d2 = self.up2(d3)
+        d2 = torch.cat([d2, e2], dim=1)
+        d2 = self.dec2(d2)
+
+        d1 = self.up1(d2)
+        d1 = torch.cat([d1, e1], dim=1)
+        d1 = self.dec1(d1)
+
+
+        return torch.sigmoid(self.final(d1))
